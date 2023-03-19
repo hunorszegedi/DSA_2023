e784302ce876eeed515de85bce753c415e452828
hunorszegedi <szegedihunorofficial@gmail.com>
2023-03-19 16:56:19 +0200

    lab5-sor/queue done

---
+++ lab5_regular_queue/headers/functions.h
@@ -0,0 +1,63 @@
+//
+// Created by Lenovo on 19/03/2023.
+//
+
+#ifndef LAB5_REGULAR_QUEUE_FUNCTIONS_H
+#define LAB5_REGULAR_QUEUE_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <constants.h>
+#include <string.h>
+#define CAR_PLATE_NUMBER_LIMIT 10
+#include <stdbool.h>
+
+typedef struct Queue{
+    int capacity;
+    int front;
+    int rear;
+    char **elements;
+}Queue;
+
+/**
+* Creates the queue data structure, allocates memory for elements
+* @param capacity
+* @param queue
+*/
+void createQueue(int capacity, Queue *queue);
+/**
+* Deallocates the elements array -> free
+* @param queue
+*/
+void destroyQueue(Queue* queue);
+/**
+* Checks whether the queue is full or not
+* @param queue
+* @return true/false
+*/
+bool isFull(Queue queue);
+/**
+* Checks whether the queue is empty or not
+* @param queue
+* @return true/false
+*/
+bool isEmpty(Queue queue);
+/**
+* Adds a new item to the end of the queue
+* @param queue
+* @param item
+*/
+void enqueue(Queue* queue, char* item);
+/**
+* Removes the first item from the queue
+* @param queue
+* @return
+*/
+char* dequeue(Queue* queue);
+/**
+* Prints all items of the queue
+* @param queue
+*/
+void display(Queue queue);
+
+#endif //LAB5_REGULAR_QUEUE_FUNCTIONS_H
---
+++ lab5_regular_queue/sources/functions.c
@@ -0,0 +1,81 @@
+//
+// Created by Lenovo on 19/03/2023.
+//
+
+#include "functions.h"
+
+
+
+void createQueue(int capacity, Queue *queue) {
+    queue->capacity = capacity;
+    queue->front = queue->rear = -1;
+    queue->elements = (char **) calloc(CAR_PLATE_NUMBER_LIMIT, sizeof(char *));
+    if (!queue->elements) {
+        printf(MEMORY_ALLOCATION_ERROR_MESSAGE);
+        exit(MEMORY_ALLOCATION_ERROR_CODE);
+    }
+    for (int i = 0; i < queue->capacity; ++i) {
+        queue->elements[i] = (char *) calloc(CAR_PLATE_NUMBER_LIMIT, sizeof(char));
+        if (!queue->elements[i]) {
+            printf(MEMORY_ALLOCATION_ERROR_MESSAGE);
+            exit(MEMORY_ALLOCATION_ERROR_CODE);
+        }
+    }
+}
+
+void destroyQueue(Queue *queue) {
+    for (int i = 0; i < queue->capacity; ++i) {
+        free(queue->elements[i]);
+    }
+    free(queue->elements);
+    queue->capacity = 0;
+    queue->front = queue->rear = -1;
+    queue = NULL;
+}
+
+bool isFull(Queue queue) {
+    return queue.rear == queue.capacity - 1;
+}
+
+bool isEmpty(Queue queue) {
+    return queue.front == -1 && queue.rear == -1;
+}
+
+void enqueue(Queue *queue, char *item) {
+    if (isFull(*queue)) {
+        printf(FULL_MESSAGE);
+        return;
+    }
+    //egyetlen elem van
+    if (queue->front == -1) {
+        queue->front = 0;
+    }
+    queue->rear++;
+    strcpy(queue->elements[queue->rear], item);
+}
+
+char *dequeue(Queue *queue) {
+    if (isEmpty(*queue)) {
+        printf(EMPTY_MESSAGE);
+        return EMPTY_MESSAGE;
+    }
+    //ha csak egyetlen elem van
+    if (queue->front == queue->rear) {
+        int poz = queue->front;
+        queue->front = queue->rear = -1;
+        return queue->elements[poz];
+    }
+    return queue->elements[queue->front++];
+}
+
+void display(Queue queue) {
+    printf("The elements of the queue:\n");
+    if(isEmpty(queue)){
+        printf(EMPTY_MESSAGE);
+        return;
+    }
+    for (int i = queue.front; i <= queue.rear; ++i) {
+        printf("\t- %s\n", queue.elements[i]);
+    }
+    printf("\n");
+}
